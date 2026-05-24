207f76b6c4e2a0c4b45ffcf24557b33be5ccdaea
chandrashekar.g <meshekar5@gmail.com>
2026-05-24 20:13:48 +0200

    Shell script

--- Day6.sh
+++ Day6.sh
@@ -4,7 +4,9 @@ Memory=$(free -h | awk '/Mem:/ {print $3}')
 echo " The current memory is: $Memory"
 
 Disk_Space=$(df -h / | awk 'NR==2 {print "Provisioned:" $2, "Used:" $3, "Available:" $4}')
-if [ $? eq 0 ] then;
+
+if [ $? -eq 0 ]
+then
     echo "Command executed successfully"
     echo "$Disk_Space"
 else
