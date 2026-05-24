f41fdd38bb3739233aefc6d9b7bcc05b0a698da5
chandrashekar.g <meshekar5@gmail.com>
2026-05-24 20:09:58 +0200

    Shell script

--- Day6.sh
+++ Day6.sh
@@ -6,7 +6,7 @@ echo " The current memory is: $Memory"
 Disk_Space=$(df -h / | awk 'NR==2 {print "Provisioned:" $2, "Used:" $3, "Available:" $4}')
 if [$? eq 0] then;
     echo "Command executed successfully"
-else
+else;
     echo "Command failed to execute"
 fi
 
