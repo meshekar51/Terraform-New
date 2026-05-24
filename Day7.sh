af8131639ab776860570ce349a296c1cb89f7323
chandrashekar.g <meshekar5@gmail.com>
2026-05-24 20:20:54 +0200

    Shell script

---
+++ Day7.sh
@@ -0,0 +1,10 @@
+df -h / > /tmp/df_output.txt 2>/tmp/df_error.txt
+
+if [ $? -eq 0 ]
+then
+    awk 'NR==2 {print "Provisioned:" $2, "Used:" $3, "Available:" $4}' /tmp/df_output.txt
+    echo "Command executed successfully"
+else
+    echo "df command failed"
+    cat /tmp/df_error.txt
+fi
