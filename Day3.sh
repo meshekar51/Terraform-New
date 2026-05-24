6139b74852640efef483d94906f3d3d7fb39a084
chandrashekar.g <meshekar5@gmail.com>
2026-05-24 16:16:56 +0200

    Shell script

---
+++ Day3.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+# Check root access or not
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root access"
+    exit 1
+fi
+
+# first arg -> what are you trying to install
+# second arg -> exit code
+VALIDATE(){
+    if [ $2 -ne 0 ]; then
+        echo "Installing $1 is ... FAILED"
+        exit 1
+    else
+        echo "Installing $1 is ... SUCCESS"
+    fi
+}
+
+# echo "I am continuing..."
+dnf list installed httpd
+
+if [ $? -eq 0 ]; then
+    echo "httpd is already installed ... SKIPPING"
+else
+    echo "Installing HTTPD"
+    dnf install httpd -y
+    VALIDATE HTTPD $?
+fi
+
+dnf list installed nginx
+if [ $? -eq 0 ]; then
+    echo "nginx is already installed ... SKIPPING"
+else
+    echo "Installing nginx"
+    dnf install nginx -y
+    VALIDATE nginx $?
+fi
