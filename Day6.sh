1c8aedfa6bd617945bc8886d6ea5c321573427b8
chandrashekar.g <meshekar5@gmail.com>
2026-05-24 19:27:36 +0200

    Shell script

--- Day6.sh
+++ Day6.sh
@@ -3,7 +3,7 @@ Memory=$(free -h | awk '/Mem:/ {print $3}')
 
 echo " The current memory is: $Memory"
 
-Disk_Space=$(df -h / | awk '{print "Provisioned": $2, "Used": $3, "Available": $4}')
+Disk_Space=$(df -h / | awk '{print "Provisioned:" $2, "Used:" $3, "Available:" $4}')
 
 echo "Disk Space is: " $Disk_Space
 
