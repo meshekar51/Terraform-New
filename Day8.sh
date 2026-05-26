0333f1e1339a1d95aa130afd405bc6b4bd9ae05f
chandrashekar.g <meshekar5@gmail.com>
2026-05-26 10:41:47 +0200

    Shell script

--- Day8.sh
+++ Day8.sh
@@ -6,7 +6,7 @@ echo "##------------------##"
 #Later add for loop
 #later add conditional statement under Loop
 #
-NGINX_LOGS=$(find /var/log/nginx -name "*.log" | xargs grep "Safari")
+NGINX_LOGS=$(find /var/log/nginx -name "*.log" | xargs grep "Safari" | tail -2)
 echo "These are the Logs from Access and Error Logs"
 echo "$NGINX_LOGS"
 
