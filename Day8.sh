1f7199e754e0cd75c2349eb25bc574419d9808c2
chandrashekar.g <meshekar5@gmail.com>
2026-05-26 09:45:22 +0200

    Shell script

---
+++ Day8.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "##------------------##"
+
+#find logs in /var/log/nginx and in files check the Public IP save it as output
+#Later add for loop
+#later add conditional statement under Loop
+#
+
+find /var/log/nginx -type f -name "*.log" -exec grep -l "Safari" {} \;
